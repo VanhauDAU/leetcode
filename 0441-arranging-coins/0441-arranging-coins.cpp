c1923199e5ba6f2393242c486de33b3b21dd2d6f
Lê Văn Hậu <137133001+VanhauDAU@users.noreply.github.com>
2026-04-12 20:34:33 +0700

    Time: 0 ms (100%), Space: 8.8 MB (66.18%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long left = 1, right = n, ans = 0;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long sum = mid * (mid + 1) / 2;
+            if (sum <= n) {
+                ans = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+};
